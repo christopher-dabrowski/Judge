0b17e8b965fd06593da155b0aa94bf872f68aa81
1stDimension <pilotpzl@gmail.com>
2018-11-12 17:25:12 +0100

    Changes:   -Adding to modules to imitate "real" players
    Signed-off-by: 1stDimension <pilotpzl@gmail.com>

---
+++ ProxyPlayer1/src/main/java/ProxyPlayer.java
@@ -0,0 +1,35 @@
+import java.util.Scanner;
+
+public class ProxyPlayer {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int heigh = Integer.parseInt(scanner.next());
+        ok();
+        String field = scanner.next();
+        String[] fields = field.split("[{};,]");
+        ok();
+        String info = scanner.next();
+        if (info.compareTo("START") == 0) {
+            int[] y = {0, 0}, x = {0, 1};
+            print(y[0]++, x[0], y[1]++, x[1]);
+            for (int i = 0; scanner.hasNext(); i++) {
+                info = scanner.next();
+                print(y[0]++, x[0], y[1]++, x[1]);
+            }
+        } else {
+            int[] y = {0, 0}, x = {0, 1};
+            for (int i = 0; scanner.hasNext(); i++) {
+                info = scanner.next();
+                print(y[0]++, x[0], y[1]++, x[1]);
+            }
+        }
+    }
+
+    private static void ok() {
+        System.out.println("ok");
+    }
+
+    private static void print(int y1, int x1, int y2, int x2) {
+        System.out.printf("{%d;%d},{%d;%d}", y1, x1, y2, x2);
+    }
+}
