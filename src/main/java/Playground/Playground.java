ef48ab39363fa09d2bcc59f10b64253b216382bf
Krzysztof Dąbrowski <k.fajny@gmail.com>
2018-12-08 00:02:09 +0100

    Generation loading and printing of obstacles

--- src/main/java/Playground/Playground.java
+++ src/main/java/Playground/Playground.java
@@ -3,6 +3,8 @@ package Playground;
 import lombok.Getter;
 import lombok.Setter;
 
+import java.util.LinkedList;
+import java.util.List;
 import java.util.Random;
 
 @Getter
@@ -10,6 +12,8 @@ public class Playground {
     private Tile[][] map;
     private int size;
     private long[] seeds; // y , x ...
+    @Getter //For creating second map with same obstacles
+    private Obstacle[] obstacles;
     @Setter
     private float percentageOfObstacles = 0.1f;
 
@@ -25,6 +29,61 @@ public class Playground {
         }
     }
 
+    @org.jetbrains.annotations.Contract(pure = true)
+    private int getNumberOfObstacles() {
+        return Math.round((size * size) * percentageOfObstacles);
+    }
+
+    /**
+     * Generate list of obstacles on the map
+     * @return List of obstacles formatted for communication
+     */
+    public String printObstacles() {
+        StringBuilder resultBuilder = new StringBuilder();
+
+        for (int i=0; i<obstacles.length; i++)
+            resultBuilder.append(obstacles[i].toString() + (i != obstacles.length-1 ? "," : ""));
+
+        return resultBuilder.toString();
+    }
+
+    /**
+     * Fills map with randomly generated obstacles
+     */
+    public void generateObstacles() {
+        obstacles = new Obstacle[getNumberOfObstacles()];
+        LinkedList<Integer> freeSpots = new LinkedList<Integer>(); //Maybe hash table would be faster
+
+        for (int i=0; i<size*size; i++)
+            freeSpots.add(new Integer(i)); //Project two dimensional array to one dimension
+
+        Random random = new Random();
+
+
+        for (int i=0; i<getNumberOfObstacles(); i++) { //Generate obstacles
+            int chosen = random.nextInt(freeSpots.size()); //Pick free spot
+            int pickedFiled = freeSpots.get(chosen);
+
+            freeSpots.remove(chosen); //It's no longer free
+
+            int row = pickedFiled/size;
+            int column = pickedFiled - row*size;
+
+            map[row][column].taken();
+            obstacles[i] = new Obstacle(column, row);
+        }
+    }
+
+    public void loadObstacles(Obstacle[] obstacles) {
+        this.obstacles = obstacles;
+    }
+
+    /**
+     * Older version of creating obstacles
+     *
+     * @deprecated use {@link #generateObstacles()} instead.
+     */
+    @Deprecated
     public void addObstacles() {
         int numberOfObstacles = Math.round((map.length * map.length) * percentageOfObstacles);
         seeds = new long[2 * numberOfObstacles];
