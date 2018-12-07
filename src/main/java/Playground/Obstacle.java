fd197516e4be17809ce5d81931c2c16d22b2d2d2
Krzysztof Dąbrowski <k.fajny@gmail.com>
2018-12-07 21:58:30 +0100

    Obstacle class

---
+++ src/main/java/Playground/Obstacle.java
@@ -0,0 +1,22 @@
+package Playground;
+
+import lombok.Getter;
+
+public class Obstacle {
+
+    @Getter
+    private final int x;
+    @Getter
+    private final int y;
+
+    Obstacle(int x, int y)
+    {
+        this.x = x;
+        this.y = y;
+    }
+
+    @Override
+    public String toString() {
+        return String.format("{%d;%d}", x, y);
+    }
+}
