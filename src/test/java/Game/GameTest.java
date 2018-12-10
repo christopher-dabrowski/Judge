7ecc39b066acf5dc48d0796bb92c37431443a020
Krzysztof Dąbrowski <k.fajny@gmail.com>
2018-12-10 11:53:34 +0100

    Use of new Player constructor

--- src/test/java/Game/GameTest.java
+++ src/test/java/Game/GameTest.java
@@ -9,8 +9,8 @@ import static org.junit.Assert.fail;
 
 public class GameTest {
 
-    private Player playerOne = new Player("One", "Jan", "Kowalski", "java -jar .\\ProxyPlayer.jar");
-    private Player playerTwo = new Player("Two", "John", "Bukowski", "java -jar .\\ProxyPlayer1.jar");
+    private Player playerOne = new Player("123456","One", "Jan", "Kowalski", "java -jar .\\ProxyPlayer.jar");
+    private Player playerTwo = new Player("987654","Two", "John", "Bukowski", "java -jar .\\ProxyPlayer1.jar");
     private Playground playground = new Playground();
     private Game game;
 
