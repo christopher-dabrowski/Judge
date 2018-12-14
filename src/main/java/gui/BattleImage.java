408753fa37fa6ce357cfea0caad9cc3df217e89e
1stDimension <pilotpzl@gmail.com>
2018-12-15 00:23:33 +0100

    -GUI Basics -Adding Jgoodies to pom.xml
    Signed-off-by: 1stDimension <pilotpzl@gmail.com>

---
+++ src/main/java/gui/BattleImage.java
@@ -0,0 +1,10 @@
+package gui;
+
+import java.awt.image.BufferedImage;
+
+public class BattleImage {
+    //TODO consider using inheritance
+    private BufferedImage bufferedImage;
+    private int blockWidth;
+    private int blockHeight;
+}
