bfcbf8b7d61ba24ec24e17e2c0f85edfdfa513af
1stDimension <pilotpzl@gmail.com>
2018-12-15 16:09:28 +0100

    -GameQueuing class that queues games -Adding clone method to playground class. Necessary in order to play 2 games on the same plain -Creating class GameStatistics for preserving data about players performance
    Signed-off-by: 1stDimension <pilotpzl@gmail.com>

--- src/main/java/gui/Battle.java
+++ src/main/java/gui/Battle.java
@@ -1,9 +1,11 @@
 package gui;
 
 public class Battle {
+    private BattlePreview battlePreview;
     private BattlePreview battlePreview1;
 
     private void createUIComponents() {
         // TODO: place custom component creation code here
+        battlePreview = new BattlePreview();
     }
 }
--- src/main/java/gui/BattlePreview.java
+++ src/main/java/gui/BattlePreview.java
@@ -5,13 +5,36 @@ import java.awt.*;
 
 public class BattlePreview extends JPanel {
 
+    private BattleImage battleImage;
+
     BattlePreview() {
         super();
         this.setDoubleBuffered(true);
+
+        //TODO Make it taken from file
+        //Until then its hardcoded TODO TODO TODO
+        battleImage = new BattleImage(new Dimension(1000, 500), 13);
+        this.setPreferredSize(new Dimension(battleImage.getBufferedImage().getWidth(), battleImage.getBufferedImage().getHeight()));
+        for (int i = 0; i < 13; i++)
+            battleImage.take(i, i, Color.RED);
+        for (int i = 0; i < 13; i++)
+            battleImage.take(i, 0, Color.GRAY);
+        for (int i = 0; i < 13; i++)
+            battleImage.take(0, i, Color.BLUE);
+    }
+
+    public static void main(String... args) {
+        JFrame jFrame = new JFrame("TEST" + BattlePreview.class);
+        BattlePreview battlePreview = new BattlePreview();
+        jFrame.setContentPane(battlePreview);
+        jFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        jFrame.pack();
+        jFrame.setVisible(true);
     }
 
     @Override
     public void paintComponent(Graphics graphics) {
         super.paintComponent(graphics);
+        graphics.drawImage(battleImage.getBufferedImage(), 0, 0, null);
     }
 }
