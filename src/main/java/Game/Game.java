820b21fa09afdb71829d881611a7556fc7f09e36
Krzysztof Dąbrowski <k.fajny@gmail.com>
2018-12-15 14:29:35 +0100

    Usage of Logger in Game

--- src/main/java/Game/Game.java
+++ src/main/java/Game/Game.java
@@ -4,6 +4,7 @@ import Communication.Messenger;
 import Logger.Logger;
 import MainLogic.Player;
 import Playground.Playground;
+import Playground.Block;
 import lombok.Getter;
 import lombok.NonNull;
 import lombok.RequiredArgsConstructor;
@@ -25,7 +26,6 @@ public class Game {
     private Messenger messenger;
 
     //TODO add option of time out
-    //TODO add logging moves
     //TODO consider caching the answer
     //TODO add endCommunication method in order to save CPU
     public GameResult play() throws IOException {
@@ -51,31 +51,54 @@ public class Game {
         timeTaken = oneMove(String.valueOf(playground.getSize()), playerOne);
         if (isWrong(messenger.getAnswer(), timeTaken, MessageType.INFORMATION)) {
             logger.logCommunicationState(false);
+            logger.close();
             return new GameResult(playerTwo, Setteling.CLASSIC);
         }
 
         timeTaken = oneMove(String.valueOf(playground.getSize()), playerTwo);
-        if (isWrong(messenger.getAnswer(), timeTaken, MessageType.INFORMATION))
+        if (isWrong(messenger.getAnswer(), timeTaken, MessageType.INFORMATION)) {
+            logger.logCommunicationState(true); //Firs player is fine
+            logger.logCommunicationState(false); //Second made mistake
+            logger.close();
             return new GameResult(playerOne, Setteling.CLASSIC);
+        }
         //Send obstacles
         timeTaken = oneMove(playground.printObstacles(), playerOne);
-        if (isWrong(messenger.getAnswer(), timeTaken, MessageType.INFORMATION))
+        if (isWrong(messenger.getAnswer(), timeTaken, MessageType.INFORMATION)) {
+            logger.logCommunicationState(false);
+            logger.close();
             return new GameResult(playerTwo, Setteling.CLASSIC);
+        }
+        logger.logCommunicationState(true); //Firs player responded correctly
 
         timeTaken = oneMove(playground.printObstacles(), playerTwo);
-        if (isWrong(messenger.getAnswer(), timeTaken, MessageType.INFORMATION))
+        if (isWrong(messenger.getAnswer(), timeTaken, MessageType.INFORMATION)) {
+            logger.logCommunicationState(false); //Second made mistake
+            logger.close();
             return new GameResult(playerOne, Setteling.CLASSIC);
+        }
+        logger.logCommunicationState(true); //Second player responded correctly
+
         //Start game
         while (true) {
             timeTaken = oneMove("START", playerOne);
             String answer = messenger.getAnswer();
-            if (isWrong(answer, timeTaken, MessageType.MOVE))
+
+            if (isWrong(answer, timeTaken, MessageType.MOVE)) {
+                logger.logPlayerError();
+                logger.close();
                 return new GameResult(playerTwo, Setteling.CLASSIC);
+            }
+            logger.logMove(new Block(answer)); //Assuming that answer looks like "{x1;y1},{x2;y2}"
 
             timeTaken = oneMove(answer, playerTwo);
             answer = messenger.getAnswer();
-            if (isWrong(answer, timeTaken, MessageType.MOVE))
+            if (isWrong(answer, timeTaken, MessageType.MOVE)) {
+                logger.logPlayerError();
+                logger.close();
                 return new GameResult(playerOne, Setteling.CLASSIC);
+            }
+            logger.logMove(new Block(answer));
         }
     }
 
