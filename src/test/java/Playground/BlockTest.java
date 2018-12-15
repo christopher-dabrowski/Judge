939019e28e0e2f78cf516975158e1e890ed6a380
Krzysztof Dąbrowski <k.fajny@gmail.com>
2018-12-15 14:25:35 +0100

    Test for Block(string) constructor

--- src/test/java/Playground/BlockTest.java
+++ src/test/java/Playground/BlockTest.java
@@ -9,7 +9,29 @@ public class BlockTest {
     @Test
     public void toStringTest() {
         Block block = new Block(8,7,7,9);
+        String expected = "{7;8},{9;7}";
 
-        System.out.println(block.toString());
+        if (!expected.equals(block.toString()))
+            fail();
+
+        //System.out.println(block.toString());
+    }
+
+    @Test
+    public void BlockConstructFromString() {
+        Block block = new Block("{54;11},{11;72}");
+
+        String expected = "{54;11},{11;72}";
+        if (!expected.equals(block.toString()))
+            fail();
+
+        try {
+            new Block("{12,24};{22;12}"); //Colon and semicolon swapped
+            fail("Expected exception to be thrown");
+        }
+        catch (IllegalArgumentException e) {
+        }
+
+        //System.out.println(block.toString());
     }
 }
