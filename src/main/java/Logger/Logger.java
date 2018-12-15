199fca653dfc66bb5e8f49019d40b48eddc7b23a
Krzysztof Dąbrowski <k.fajny@gmail.com>
2018-12-15 13:09:17 +0100

    Added logging the fact whether player responded to the initial message

--- src/main/java/Logger/Logger.java
+++ src/main/java/Logger/Logger.java
@@ -20,15 +20,19 @@ import java.io.IOException;
  * Obstacles: {1;2},{3;4},{5,7}
  * Player 1: ToBolekxx
  * Player 2: SuperGracz232
+ * OK
+ * OK
  * Moves:
  * {7;8},{7;8}
  * {0,0},{1;0}
+ * ERROR
  */
 public class Logger {
     private static String LOGS_FOLDER_NAME = "game_logs";
 
     private final BufferedWriter output;
     private final String fileName; //<Player1>VS<Player2>.log
+    private boolean loggingMoves = false; //At the beginning Logger needs to log player data. Then it switches to moves
 
     /**
      * Starts to log game
@@ -58,14 +62,58 @@ public class Logger {
         output.write("Moves:\n");
     }
 
+    /**
+     * Logs weather player responded for the initial message.
+     * Should be called only once for each player!
+     * @param playerResponded Information whether the player responded to initial message or not
+     * @throws IOException
+     */
+    public void logCommunicationState(boolean playerResponded) throws IOException {
+        if (playerResponded)
+            output.write("OK\n");
+        else
+            output.write("ERROR\n");
+    }
+
+    /**
+     * Logs one move of player
+     * @param blockPlaced Block that player placed
+     * @throws IOException
+     */
     public void logMove(Block blockPlaced) throws IOException {
+        if (!loggingMoves) { //Starts to log moves
+            loggingMoves = true;
+            output.write("Moves:");
+        }
+
         output.write(blockPlaced.toString() + "\n");
     }
 
+    /**
+     * Logs one move of player
+     * @param y1 First tile of block
+     * @param x1 First tile of block
+     * @param y2 Second tile of block
+     * @param x2 Second tile of block
+     * @throws IOException
+     */
     public void logMove(int y1, int x1, int y2, int x2) throws IOException {
+        if (!loggingMoves) { //Starts to log moves
+            loggingMoves = true;
+            output.write("Moves:");
+        }
+
         Block blockPlaced = new Block(y1, x1, y2, x2);
 
         output.write(blockPlaced.toString() + "\n");
+    }
+
+    /**
+     * Logs that player did't make correct move
+     * @throws IOException
+     */
+    public void logPlayerError() throws IOException {
+        output.write("ERROR\n");
     }
 
     /**
