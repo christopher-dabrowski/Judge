79dd9a8014537c3b52ef194103119e1e8e97bdc2
Krzysztof Dąbrowski <k.fajny@gmail.com>
2018-12-15 14:09:31 +0100

    Block(string) constructor

--- src/main/java/Playground/Block.java
+++ src/main/java/Playground/Block.java
@@ -3,13 +3,43 @@ package Playground;
 import lombok.Getter;
 import javafx.util.Pair;
 
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 /**
  * Class tha represents 2x1 block
  */
 public class Block {
     private final Pair<Part, Part> parts;
 
+    /**
+     * Creat block from coordinates
+     * @param y1 First tile of block
+     * @param x1 First tile of block
+     * @param y2 Second tile of block
+     * @param x2 Second tile of block
+     */
     public Block(int y1, int x1, int y2, int x2) {
+        parts = new Pair<>(new Part(x1, y1), new Part(x2, y2));
+    }
+
+    /**
+     * Create block from it's string representation
+     * @param block Block as a string "{x1;y1},{x2;y2}"
+     */
+    public Block(String block) throws IllegalArgumentException {
+        Pattern regex = Pattern.compile("\\{(\\d+);(\\d+)},\\{(\\d+);(\\d+)}");
+        Matcher matcher = regex.matcher(block);
+
+        if (!matcher.find()) {
+            throw new IllegalArgumentException("Invalid block format");
+        }
+
+        int x1 = Integer.parseInt(matcher.group(1));
+        int y1 = Integer.parseInt(matcher.group(2));
+        int x2 = Integer.parseInt(matcher.group(3));
+        int y2 = Integer.parseInt(matcher.group(4));
+
         parts = new Pair<>(new Part(x1, y1), new Part(x2, y2));
     }
 
