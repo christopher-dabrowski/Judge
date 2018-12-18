d91e4c4f783ecfff68109ba8ac813e5c042fcf9f
Krzysztof Dąbrowski <k.fajny@gmail.com>
2018-12-18 21:36:50 +0100

    Fix: Close parsing player file also when error detected

--- src/main/java/parser/Parser.java
+++ src/main/java/parser/Parser.java
@@ -18,27 +18,42 @@ public class Parser {
 
         if (input.hasNext())
             alias = input.nextLine();
-        else throw new ParseException("No info found", 0);
+        else {
+            input.close();
+            throw new ParseException("No info found", 0);
+        }
 
         if (input.hasNext()) {
             name = input.next();
             if (!name.matches("\\p{L}+")) throw new ParseException("Invalid player name", 1);
-        } else throw new ParseException("No player name found", 1);
+        } else {
+            input.close();
+            throw new ParseException("No player name found", 1);
+        }
 
         if (input.hasNext()) {
             surname = input.next();
             if (!surname.matches("\\p{L}+")) throw new ParseException("Invalid player surname", 2);
-        } else throw new ParseException("No player surname found", 2);
+        } else {
+            input.close();
+            throw new ParseException("No player surname found", 2);
+        }
 
         //Finish line with name and surname
         if (input.hasNext()) {
             input.nextLine();
-        } else throw new ParseException("New line after surname expected", 2);
+        } else {
+            input.close();
+            throw new ParseException("New line after surname expected", 2);
+        }
 
         if (input.hasNext()) {
             lunchCommand = input.nextLine();
             if (lunchCommand.length() <= 0) throw new ParseException("No lunch command", 3);
-        } else throw new ParseException("No lunch command", 3);
+        } else {
+            input.close();
+            throw new ParseException("No lunch command", 3);
+        }
 
         input.close();
         return new Player(indexNumber, alias, name, surname, lunchCommand);
