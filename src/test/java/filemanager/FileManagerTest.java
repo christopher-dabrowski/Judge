3054ee13c841df1faa6b688c0748b7b12c7caf39
Krzysztof Dąbrowski <k.fajny@gmail.com>
2018-12-18 22:10:31 +0100

    Proper unit test for importPlayers()

--- src/test/java/filemanager/FileManagerTest.java
+++ src/test/java/filemanager/FileManagerTest.java
@@ -12,7 +12,7 @@ import java.util.ArrayList;
 import java.io.File;
 import java.util.HashMap;
 
-import static org.junit.Assert.fail;
+import static org.junit.Assert.*;
 
 public class FileManagerTest {
     private static String tempFolderName = "TEMP";
@@ -72,21 +72,26 @@ public class FileManagerTest {
     public void tearDown() throws Exception {
         deleteTemporaryPlayersData(playerIndexesAndInfoFiles);
 
+        if (!new File(tempFolderName).delete())
+            fail();
     }
 
-    //ToDo Write solid tests
     @Test
     public void importPlayers() {
         try {
             ArrayList<String> errors = new ArrayList<String>();
             ArrayList<Player> players = FileManager.importPlayers(tempFolderName, errors);
 
-            for (Player player : players) {
-                System.out.println(player);
-            }
-            System.out.println("Errors:");
-            for (String s : errors)
-                System.out.println(s);
+            assertEquals(2, players.size());
+            assertArrayEquals(new String[] {"Folder 999998 is missing info.txt file", "Error when parsing info.txt form folder 999999 Error: Invalid player name"}, errors.toArray());
+
+//            for (Player player : players) {
+//                System.out.println(player);
+//            }
+
+//            System.out.println("Errors:");
+//            for (String s : errors)
+//                System.out.println(s);
         } catch (Exception e) {
             fail();
         }
