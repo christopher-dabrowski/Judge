afd54fae31103e3edc436e9832c959ca978354aa
Krzysztof Dąbrowski <k.fajny@gmail.com>
2018-12-26 14:54:19 +0100

    Logo and button

--- src/main/java/gui/GUIMain.java
+++ src/main/java/gui/GUIMain.java
@@ -4,14 +4,17 @@ import javax.swing.*;
 
 public class GUIMain {
     private JPanel mainPanel;
-    private JLabel TESTLabel;
+    private JButton startButton;
 
     public static void main(String... args) {
         JFrame jFrame = new JFrame("Judge");
         GUIMain gui = new GUIMain();
         jFrame.setContentPane(gui.mainPanel);
         jFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        jFrame.pack();
+
+        jFrame.setSize(800, 600);
+
+        //jFrame.pack();
         jFrame.setVisible(true);
     }
 }
