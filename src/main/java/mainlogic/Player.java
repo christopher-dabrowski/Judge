8a3b5aa25b4eee55bdd9bee3d8bd97d417dfe7e0
Krzysztof Dąbrowski <k.fajny@gmail.com>
2018-12-30 11:15:51 +0100

    New field in player for full program paht

--- src/main/java/mainlogic/Player.java
+++ src/main/java/mainlogic/Player.java
@@ -11,15 +11,28 @@ public class Player {
     private final String name;
     @Getter
     private final String surname;
+    /**
+     * Exact string that player put in his info.txt file
+     */
     @Getter
     private final String lunchCommand;
+    /**
+     * Lunch command with full path so that it can be easily run
+     */
+    @Getter
+    private final String fullLunchCommand;
 
     public Player(String indexNumber, String alias, String name, String surname, String lunchCommand) {
+        this(indexNumber, alias, name, surname, lunchCommand, null);
+    }
+
+    public Player(String indexNumber, String alias, String name, String surname, String lunchCommand, String fullLunchCommand) {
         this.indexNumber = indexNumber;
         this.alias = alias;
         this.name = name;
         this.surname = surname;
         this.lunchCommand = lunchCommand;
+        this.fullLunchCommand = fullLunchCommand;
     }
 
     @Override
