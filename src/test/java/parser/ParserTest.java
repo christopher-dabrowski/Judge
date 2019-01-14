00aa207de258552640790f79552434707f943ba1
Krzysztof Dabrowski <01133318@pw.edu.pl>
2019-01-14 18:43:45 +0100

    Test for java files

--- src/test/java/parser/ParserTest.java
+++ src/test/java/parser/ParserTest.java
@@ -1,14 +1,12 @@
 package parser;
 
+import filemanager.FileManager;
 import mainlogic.Player;
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 
-import java.io.File;
-import java.io.FileNotFoundException;
-import java.io.PrintWriter;
-import java.io.UnsupportedEncodingException;
+import java.io.*;
 
 import static org.junit.Assert.fail;
 
@@ -21,7 +19,10 @@ public class ParserTest {
     public void setUp() /*throws Exception*/ {
         new File(tempFolderName).mkdir();
         new File(tempPlayerFolderName).mkdir();
+    }
 
+    @Test
+    public void readPlayerInfo() {
         try {
             PrintWriter writer = new PrintWriter(temFileName, "UTF-8");
             writer.println("Kserkses");
@@ -31,10 +32,7 @@ public class ParserTest {
         } catch (FileNotFoundException | UnsupportedEncodingException exception) {
             fail();
         }
-    }
 
-    @Test
-    public void readPlayerInfo() {
         Player player = null;
 
         try {
@@ -46,6 +44,37 @@ public class ParserTest {
         //if (player == null) fail();
 
         if (!player.toString().equals("Kserkses Aleksander Dobrowolski fajny_program.exe")) fail();
+    }
+
+    @Test
+    public void appendJavaPath() throws IOException {
+        try {
+            PrintWriter writer = new PrintWriter(temFileName, "UTF-8");
+            writer.println("Kserkses");
+            writer.println("Aleksander Dobrowolski");
+            writer.println("java -jar proGram.jar");
+            writer.close();
+        } catch (FileNotFoundException | UnsupportedEncodingException exception) {
+            fail();
+        }
+
+        Player player = null;
+
+        try {
+            player = Parser.readPlayerInfo(temFileName);
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+            fail();
+        }
+
+        System.out.println(FileManager.getProgramLocation());
+        if (!player.getFullLunchCommand().matches("java -jar [A-Z]:\\\\.*\\\\\\d{6}\\\\.*.jar"))
+            fail();
+    }
+
+    @Test
+    public void appendExePath() {
+
     }
 
     @After
